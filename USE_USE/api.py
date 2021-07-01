7f8ff62ea901881135beb0c99c479af48a68e62f
Kuldeep.Dhakar <kuldeep.dhakar@peoplestrong.com>
2021-07-02 03:08:25 +0530

    First commit with all code

---
+++ USE_USE/api.py
@@ -0,0 +1,12 @@
+import requests
+import json
+title = "Joy Honey & Almonds Advanced Nourishing Body Lotion, For Normal to Dry skin"
+text = "The lotion inside the bottle is not the same quality as the make of Joy. It clearly seemed much more less creamy and not smelling good at all."
+# interest_list = ['maths']
+data = {'title': title, 'text': text}
+
+
+res = requests.post(url="http://0.0.0.0:7000/api/classify_products", json=data)
+
+print(json.dumps(res.json(), indent=4))
+json.dump(res.json(), open("response.json", "w"), indent=4)
